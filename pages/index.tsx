22582761a34baa479a7f91898e87e7284d631e26
Cesar Peres <cperes84@gmail.com>
2024-03-08 16:56:21 -0300

    chore: adicionar estrelas

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,29 +58,34 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           All diary games chalenge in one place!
         </p>
+
         <div className={styles.grid}>
-          {games.map(({ url, title, description, image, tags }) => (
+          {games.map(({ url, title, description, image, tags, stars }) => (
               <a href={url} target="_blank" className={styles.card}>
-              {image && <img src={image} width="240" height="140" />}
-                <h2>{title}</h2>
-                <p>{description}</p>
-                {tags.length ? tags.map((item) => <span className={styles.tag}>{item}</span>) : null}
+                  <div style={{ textAlign: 'right', width: '240px', margin: 0, padding: 0 }}>{stars > 0 && (new Array(stars).fill(null)).map(() => <img src="images/star.svg" width="16" style={{ marginLeft: '4px' }} />)}</div>
+                  <div style={{ minHeight: "290px" }}>
+                    {image && <img src={image} width="240" height="140" />}
+                    <h2>{title}</h2>
+                    <p style={{ fontSize: "16px" }}>{description}</p>
+                  </div>
+                  <div>{tags.length ? tags.map((item) => <span className={styles.tag}>{item}</span>) : null}</div>
               </a>
           ))}
           </div>
+
           <div className={styles.container}>
-          <div className={styles.about}>
-          <h3>About Daily Games</h3>
-<p>If you like to play games that offer new content or puzzle each day? Then you came to the right place and definitely daily games are for you in one place! Whether you enjoy daily crosswords, daily sudoku, or daily jigsaws. 
-This type of games help you to improve memory and mood. So, let's go and play daily game today and tomorrow!</p>
+            <div className={styles.about}>
+              <h3>About Daily Games</h3>
+                <p> You like to play games that offer new content or puzzle each day? Then you came to the right place and definitely daily games are for you in one place! Whether you enjoy daily crosswords, daily sudoku, or daily jigsaws. 
+                This type of games help you to improve memory and mood. So, let's go and play daily game today and tomorrow!</p>
 
-<p>Fun facts about Daily Games:</p>
-<p>Daily games are awesome because players enjoy playing each day's new puzzle and awesome content.
-The are totally free daily games and are inspired in classic games.
-Most of then get more challenging as the week goes on; for example, a Monday game is the easiest, and a Sunday game is the most challenging.</p>
+                <h3>Fun facts about Daily Games</h3>
+                <p>Daily games are awesome because players enjoy playing each day's new puzzle and awesome content.
+                The are totally free daily games and are inspired in classic games.
+                Most of then get more challenging as the week goes on; for example, a Monday game is the easiest, and a Sunday game is the most challenging.</p>
+            </div>
           </div>
 
-          </div>
       </main>
       <div className={styles.container}>
       <footer className={styles.footer}>
